e9dbae7b650dcedb09dce8637096a0f690d7291b
Biya Kazmi <kazmib@mcmaster.ca>
2020-06-03 19:06:12 -0400

    Duplicate Min finder

--- FindMin.py
+++ FindMin.py
@@ -38,4 +38,57 @@ class Solution(object):
                 if prev > nums[i] and after > nums[i]:
                     return nums[i]
                 else: i = i + 1
+    def findDupMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #remove duplicates in linear time.
+        dic = {}
         
+        nodup = []
+        
+        for i in range(len(nums)):
+            if nums[i] not in dic:
+                dic[nums[i]] = 1
+                nodup = nodup + [nums[i]]
+            
+        nums = nodup
+        print(nums)
+        
+        #min element is one that has a large lement before and after
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            if nums[0] >= nums[1]:
+                return nums[1]
+            else: return nums[0]
+        
+        i = 0
+        
+        while i < len(nums):
+            if i == 0:
+                prev = nums[-1]
+                after = nums[1]
+                if prev > nums[i] and after > nums[i]:
+                    #this is min bitch 
+                    return nums[i]
+                else: i = i + 1
+                    
+            elif i == len(nums)-1:
+                prev = nums[i-1]
+                after = nums[0]
+                if prev > nums[i] and after > nums[i]:
+                    return nums[i]
+                else: i = i + 1
+                    
+            else:
+                prev = nums[i-1]
+                after = nums[i+1]
+                if prev > nums[i] and after > nums[i]:
+                    return nums[i]
+                else: i = i + 1
+        
+
